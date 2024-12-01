1deeffc25d454428c81004d51f03a2b28c8f60c2
Tomás <tsyf1999@gmail.com>
2024-12-01 00:55:23 -0300

    explicit plugin

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -2,7 +2,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
 import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, DefineComponent, h } from 'vue';
+import { createSSRApp, DefineComponent, h, Plugin } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -19,7 +19,7 @@ createServer((page) =>
             ),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
-                .use(plugin)
+                .use(plugin as Plugin)
                 .use(ZiggyVue, {
                     ...page.props.ziggy,
                     location: new URL(page.props.ziggy.location),
